3f446cfbcd3f4d65701e8798d73e9bb593ac60c2
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-01-31 07:33:28 +0000

    Booking Form Updates

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
 
-let form = document.getElementById("form");
-let firstName = document.getElementById("firstname");
-let lastName = document.getElementById("lastname");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  console.log("yoyoyo");
-  console.log("The first Name is ",firstName.value, "The Last name is",lastName.value);
-});
-
+let firstName = document.querySelector("#firstname");
+let userInput = prompt("what is your first name");
+firstName.textContent= userInput;
