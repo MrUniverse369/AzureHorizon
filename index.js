e6388cc6c0e8ff04e770133247c6ab02f00cd8f8
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-05-31 07:56:50 +0100

    form can now accept and proccess user input

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+const addForm = document.forms['add-form'];
 
-let firstName = document.querySelector("#firstName");
+addForm.addEventListener('submit',function(e){
+    e.preventDefault()
+    const fName = document.getElementById("Fname").value;
+    const lName = document.getElementById("Lname").value;
+    const addLine = document.getElementById("addLine").value;
+    const townCity = document.getElementById("townCity").value;
+    const pCOde = document.getElementById("pCode").value;
+    const cSelect = document.getElementById("cSelect").value;
+    const eMail = document.getElementById("Email").value;
+    const pNum = document.getElementById("Pnum").value;
+    const pSelect = document.getElementById("pSelect").value;
+    console.log(fName);
+    console.log(lName);
+    console.log(addLine);
+    console.log(townCity);
+    console.log(pCOde);
+    console.log(cSelect);
+    console.log(pSelect);
+    console.log(eMail);
+    console.log(pNum);
+    alert("Your "+pSelect+" Package has been booked a confirmation email should be with you shortly");
+    
+});
+    
 
-firstName.textContent= userInput;
