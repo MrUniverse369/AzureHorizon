62648248da523f1bf1c2a29c376f91c9e2b3de0f
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-06-16 09:26:46 +0100

    Client input validation added to booking form

--- index.js
+++ index.js
@@ -1,25 +1,126 @@
 const addForm = document.forms['add-form'];
+const fName = document.getElementById("Fname");
 
-addForm.addEventListener('submit',function(e){
-    e.preventDefault()
-    const fName = document.getElementById("Fname").value;
-    const lName = document.getElementById("Lname").value;
-    const addLine = document.getElementById("addLine").value;
-    const townCity = document.getElementById("townCity").value;
-    const pCOde = document.getElementById("pCode").value;
-    const cSelect = document.getElementById("cSelect").value;
-    const eMail = document.getElementById("Email").value;
-    const pNum = document.getElementById("Pnum").value;
-    const pSelect = document.getElementById("pSelect").value;
-    console.log(fName);
-    console.log(lName);
-    console.log(addLine);
-    console.log(townCity);
-    console.log(pCOde);
-    console.log(cSelect);
-    console.log(pSelect);
-    console.log(eMail);
-    console.log(pNum);
-    alert("Hey "+fName+" "+lName+" we have received your booking for "+pSelect+" one of our expert resort specialists will be in touch about next steps.\n" );});
+    const lName = document.getElementById("Lname");
+    const Badd = document.getElementById("Badd");
+    const townCity = document.getElementById("townCity");
+    const pCOde = document.getElementById("pCode");
+    const cSelect = document.getElementById("cSelect");
+    const eMail = document.getElementById("Email");
+    const pNum = document.getElementById("Pnum");
+    const pSelect = document.getElementById("pSelect");
+  
+    const fNameErr = document.querySelector(".FnameErr");
+    const lNameErr = document.querySelector(".LnameErr");
+    const bAddErr = document.querySelector(".BAddErr");
+    const tcErr = document.querySelector(".TCErr");
+    const pcErr = document.querySelector(".PCErr");
+    const eMailErr = document.querySelector(".eMailErr");
+
+
+    fNameErr.style.display = "none";
+    lNameErr.style.display = "none";
+    bAddErr.style.display = "none";
+    tcErr.style.display = "none";
+    pcErr.style.display = "none";
+    eMailErr.style.display = "none";
+
+   
+    const hasNumbers = (str) => {
+        return /\d/.test(str);
+      };
+
+      function InputHasValue(){
+        if(fName.value === '' || fName.value == null){
+            fNameErr.style.display = "block"; 
+            fNameErr.style.color = "red";
+            fNameErr.innerHTML = "Field can not be empty";
+           }
+           else{
+            fNameErr.style.display = "none";
+         }
+
+
+          if(lName.value === '' || lName.value == null){
+            lNameErr.style.display = "block";
+            lNameErr.style.color = "red";
+            lNameErr.innerHTML = "Field can not be empty";
+          }
+          else{
+            lNameErr.style.display = "none";
+         }
+
+          if(Badd.value === '' || Badd.value == null){
+            bAddErr.style.display = "block";
+            bAddErr.style.color = "red";
+            bAddErr.innerHTML = "Field can not be empty";
+          }
+          else{
+            bAddErr.style.display = "none";
+         }
+
+          if(townCity.value === '' || townCity.value == null){
+          tcErr.style.display = "block";
+          tcErr.style.color = "red";
+          tcErr.innerHTML = "Field can not be empty";
+          }
+          else{
+            tcErr.style.display = "none";
+         }
+       
+          if(pCOde.value === '' || pCOde.value == null){
+            pcErr.style.display = "block"; 
+            pcErr.style.color = "red";
+            pcErr.innerHTML = "Field can not be empty";
+        }
+        else{
+            pcErr.style.display = "none";
+         }
+       
+       
+      }
+
+      function InputHasNumbers(){
+
+        fNameHasNumbers = hasNumbers(fName.value);
+        lNameHasNumbers = hasNumbers(lName.value);
+
+        if(fNameHasNumbers){ 
+         fNameErr.style.display = "block";
+         fNameErr.style.color = "red";
+         fNameErr.innerHTML = "First name can not contain numbers";
+         }
     
+
+         if(lNameHasNumbers){
+          lNameErr.style.display = "block";
+          lNameErr.style.color = "red";
+          lNameErr.innerHTML = "last name can not contain numbers";
+         }
+         
+
+      }
+
+     function isEmailValid(){
+        const emailVal = eMail.value.match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+if(!emailVal){
+
+        eMailErr.style.display = "block";
+        eMailErr.style.color ="red";
+        eMailErr.innerHTML =  "Please enter a valid email";
+    }
+    else{
+        eMailErr.style.display = "none";
+    }
+}
+      
+addForm.addEventListener('submit',function(e){
+    e.preventDefault();
+    InputHasValue();
+    InputHasNumbers();
+    isEmailValid();
+});
+ 
 
